66c51e98e83393f3f0c4225efdcdb15673910bd6
sarbagyastha <sarbagyastha@gmail.com>
2022-11-03 13:16:05 +0545

    :sparkles: added loading indicator

--- khalti-android/src/main/java/com/khalti/android/PaymentActivity.kt
+++ khalti-android/src/main/java/com/khalti/android/PaymentActivity.kt
@@ -10,6 +10,7 @@ import android.view.Gravity
 import android.webkit.*
 import android.widget.LinearLayout
 import android.widget.LinearLayout.LayoutParams
+import android.widget.ProgressBar
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -25,6 +26,10 @@ internal class PaymentActivity : Activity() {
 
         val appBar = AppBarLayout(this)
         val toolbar = MaterialToolbar(this)
+        
+        val progressBar = ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal)
+        progressBar.isIndeterminate = true
+
         toolbar.title = "Pay with Khalti"
         toolbar.setNavigationIcon(com.google.android.material.R.drawable.abc_ic_ab_back_material)
         toolbar.setNavigationOnClickListener {
@@ -45,12 +50,19 @@ internal class PaymentActivity : Activity() {
             intent.getParcelableExtra(OpenKhaltiPay.CONFIG)
         }?.let { it ->
             webView.webViewClient = EPaymentWebClient(this, it.returnUrl)
+            webView.webChromeClient = object : WebChromeClient() {
+                override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                    progressBar.visibility = if (newProgress == 100) ProgressBar.GONE else ProgressBar.VISIBLE
+                }
+            }
+
             webView.loadUrl(it.paymentUrl)
         }
 
         appBar.addView(toolbar)
 
         layout.addView(appBar)
+        layout.addView(progressBar)
         layout.addView(webView, params)
 
         setContentView(layout, params)
