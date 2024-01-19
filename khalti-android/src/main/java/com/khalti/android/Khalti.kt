367a5625c7269595741f445ac1b0d2beee2cfc21
Ishwor-Shrestha <ishworisgod@gmail.com>
2024-01-19 12:31:49 +0545

    Update gradle

--- khalti-android/src/main/java/com/khalti/android/Khalti.kt
+++ khalti-android/src/main/java/com/khalti/android/Khalti.kt
@@ -4,19 +4,23 @@
 
 package com.khalti.android
 
-class Khalti private constructor(
+class KhaltiPayConfig private constructor(
     val paymentUrl: String,
     val returnUrl: String,
     val openInKhalti: Boolean,
+    val autoVerify: Boolean,
     val publicKey: String?,
     val merchantAppDeeplink: String?,
+    val onComplete: OnComplete?,
 ) {
     class Builder {
         private var publicKey: String? = null
         private var paymentUrl: String? = null
         private var returnUrl: String? = null
-        private var openInKhalti: Boolean? = null
+        private var openInKhalti: Boolean = true
+        private var autoVerify: Boolean = true
         private var merchantAppDeeplink: String? = null
+        private var onComplete: OnComplete? = null
 
         fun publicKey(publicKey: String): Builder {
             this.publicKey = publicKey
@@ -38,29 +42,35 @@ class Khalti private constructor(
             return this
         }
 
+        fun autoVerify(autoVerify: Boolean): Builder {
+            this.autoVerify = autoVerify
+            return this
+        }
+
         fun merchantAppDeeplink(merchantAppDeeplink: String): Builder {
             this.merchantAppDeeplink = merchantAppDeeplink
             return this
         }
 
-        fun build(): Khalti {
+        fun onPaymentComplete(onComplete: OnComplete): Builder {
+            this.onComplete = onComplete
+            return this
+        }
+
+        fun build(): KhaltiPayConfig {
             assert(paymentUrl != null) { "Payment url is required" }
             assert(returnUrl != null) { "Return url is required" }
 
-            val openInKhalti: Boolean = if (this.openInKhalti == null) {
-                true
-            } else {
-                this.openInKhalti!!
-            }
-
             assert(openInKhalti || merchantAppDeeplink != null) { "Merchant app's deeplink is required when [openInKhalti] is true" }
 
-            return Khalti(
+            return KhaltiPayConfig(
                 paymentUrl = this.paymentUrl!!,
                 returnUrl = returnUrl!!,
                 openInKhalti = openInKhalti,
+                autoVerify = true,
                 merchantAppDeeplink = this.merchantAppDeeplink,
-                publicKey = this.publicKey
+                publicKey = this.publicKey,
+                onComplete = this.onComplete
             )
         }
     }
