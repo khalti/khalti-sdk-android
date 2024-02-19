dcad2920b83cda62e9a04d45c1a62903f5550f21
Ishwor-Shrestha <ishworisgod@gmail.com>
2024-02-19 11:11:38 +0545

    Replace builder pattern with overloaded functions init

--- khalti-android/src/main/java/com/khalti/android/v3/Khalti.kt
+++ khalti-android/src/main/java/com/khalti/android/v3/Khalti.kt
@@ -2,96 +2,108 @@
  * Copyright (c) 2024. The Khalti Authors. All rights reserved.
  */
 
-package com.khalti.android
+package com.khalti.android.v3
+
+import android.net.Uri
 
 // Though kotlin provides named and optional parameters
-// builder pattern was required for Java developers
+// method overloading was required for Java developers
 class Khalti private constructor(
-    val paymentUrl: String,
-    val returnUrl: String,
-    val openInKhalti: Boolean,
-    val autoVerify: Boolean,
     val publicKey: String,
-    val merchantAppDeeplink: String,
-    val onComplete: OnComplete,
+    val paymentUrl: Uri,
+    val returnUrl: Uri,
+    val openInKhalti: Boolean,
+    val onPaymentResult: OnPaymentResult,
+    val onMessage: OnMessage,
+    val onReturn: OnReturn?,
 ) {
-    class Builder {
-        private var publicKey: String? = null
-        private var paymentUrl: String? = null
-        private var returnUrl: String? = null
-        private var openInKhalti: Boolean = true
-        private var autoVerify: Boolean = true
-        private var merchantAppDeeplink: String? = null
-        private var onComplete: OnComplete? = null
-
-        fun publicKey(publicKey: String): Builder {
-            this.publicKey = publicKey
-            return this
-        }
-
-        fun paymentUrl(paymentUrl: String): Builder {
-            this.paymentUrl = paymentUrl
-            return this
-        }
-
-        fun returnUrl(returnUrl: String): Builder {
-            this.returnUrl = returnUrl
-            return this
-        }
-
-        fun openInKhalti(openInKhalti: Boolean): Builder {
-            this.openInKhalti = openInKhalti
-            return this
-        }
-
-        fun autoVerify(autoVerify: Boolean): Builder {
-            this.autoVerify = autoVerify
-            return this
-        }
-
-        fun merchantAppDeeplink(merchantAppDeeplink: String): Builder {
-            this.merchantAppDeeplink = merchantAppDeeplink
-            return this
-        }
-
-        fun onPaymentComplete(onComplete: OnComplete): Builder {
-            this.onComplete = onComplete
-            return this
-        }
-
-        fun build(): Khalti {
-            val assertionConditions: Map<String, Boolean> = mapOf(
-                "onComplete" to (onComplete != null),
-                "paymentUrl" to (paymentUrl != null),
-                "returnUrl" to (returnUrl != null),
-                "publicKey" to (publicKey != null),
-                "merchantAppDeeplink" to (merchantAppDeeplink != null),
-            ).filter { !it.value }
-
-            assert(assertionConditions.isEmpty())
-            {
-                val requiredParams = assertionConditions.keys.joinToString(separator = ", ")
-                val requiredParamsSize = assertionConditions.size
-                "$requiredParams ${if (requiredParamsSize > 1) "are" else "is"} required"
-            }
-
+    companion object {
+        fun init(
+            publicKey: String,
+            paymentUrl: Uri,
+            returnUrl: Uri,
+            openInKhalti: Boolean,
+            onPaymentResult: OnPaymentResult,
+            onMessage: OnMessage,
+            onReturn: OnReturn,
+        ): Khalti {
             return Khalti(
-                paymentUrl = paymentUrl!!,
-                returnUrl = returnUrl!!,
-                openInKhalti = openInKhalti,
-                autoVerify = autoVerify,
-                merchantAppDeeplink = merchantAppDeeplink!!,
-                publicKey = publicKey!!,
-                onComplete = onComplete!!
+                publicKey,
+                paymentUrl,
+                returnUrl,
+                openInKhalti,
+                onPaymentResult,
+                onMessage,
+                onReturn,
+            )
+        }
+
+        fun init(
+            publicKey: String,
+            paymentUrl: Uri,
+            returnUrl: Uri,
+            onPaymentResult: OnPaymentResult,
+            onMessage: OnMessage,
+            onReturn: OnReturn,
+        ): Khalti {
+            return Khalti(
+                publicKey,
+                paymentUrl,
+                returnUrl,
+                true,
+                onPaymentResult,
+                onMessage,
+                onReturn,
+            )
+        }
+
+        fun init(
+            publicKey: String,
+            paymentUrl: Uri,
+            returnUrl: Uri,
+            openInKhalti: Boolean,
+            onPaymentResult: OnPaymentResult,
+            onMessage: OnMessage,
+        ): Khalti {
+            return Khalti(
+                publicKey,
+                paymentUrl,
+                returnUrl,
+                openInKhalti,
+                onPaymentResult,
+                onMessage,
+                null,
+            )
+        }
+
+        fun init(
+            publicKey: String,
+            paymentUrl: Uri,
+            returnUrl: Uri,
+            onPaymentResult: OnPaymentResult,
+            onMessage: OnMessage,
+        ): Khalti {
+            return Khalti(
+                publicKey,
+                paymentUrl,
+                returnUrl,
+                true,
+                onPaymentResult,
+                onMessage,
+                null,
             )
         }
     }
 
-    fun makePayment() {
+    fun open() {
 
     }
 
-    fun verifyPayment() {
+    fun verify() {
+
+    }
+
+    fun close() {
 
     }
 }
