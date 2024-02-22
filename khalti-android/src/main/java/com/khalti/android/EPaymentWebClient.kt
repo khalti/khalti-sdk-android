82aeb74f44349c1aa1acc92d1494c702a2f067f3
Ishwor-Shrestha <ishworisgod@gmail.com>
2024-02-22 11:15:55 +0545

    Remove old handleUri function

--- khalti-android/src/main/java/com/khalti/android/EPaymentWebClient.kt
+++ khalti-android/src/main/java/com/khalti/android/EPaymentWebClient.kt
@@ -2,24 +2,22 @@
 
 package com.khalti.android
 
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.webkit.*
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 
 internal class EPaymentWebClient : WebViewClient() {
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?):
-            Boolean = handleUriV3(view, request!!.url)
+            Boolean = handleUri(view, request!!.url)
 
     @SuppressWarnings("deprecation")
     @Deprecated("")
     override fun shouldOverrideUrlLoading(view: WebView?, url: String?):
-            Boolean = handleUriV3(view, Uri.parse(url))
+            Boolean = handleUri(view, Uri.parse(url))
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceivedError(
@@ -37,7 +35,7 @@ internal class EPaymentWebClient : WebViewClient() {
         failingUrl: String?
     ) = handleError(description)
 
-    private fun handleUriV3(view: WebView?, uri: Uri) : Boolean {
+    private fun handleUri(view: WebView?, uri: Uri) : Boolean {
         val url = uri.toString()
         val path = uri.path
         val fragment = uri.fragment
@@ -62,39 +60,5 @@ internal class EPaymentWebClient : WebViewClient() {
 
         activity.setResult(OpenKhaltiPay.PAYMENT_URL_LOAD_ERROR, intent)
         activity.finish()
-    }
-
-    private fun handleUri(view: WebView?, uri: Uri): Boolean {
-        val url = uri.toString()
-        val path = uri.path
-        val fragment = uri.fragment
-
-        val eBankingPath = "/ebanking/initiate/"
-        val mPinPath = "/account/transaction_pin"
-
-        if (url == OpenKhaltiPay.DEFAULT_HOME) {
-            activity.finish()
-        } else if (url.startsWith(returnUrl)) {
-            val isSuccess = uri.getQueryParameter("pidx") != null
-            val intent = Intent()
-            intent.putExtra(OpenKhaltiPay.RESULT, url)
-
-            activity.setResult(
-                if (isSuccess) Activity.RESULT_OK else OpenKhaltiPay.ERROR,
-                intent
-            )
-            activity.finish()
-        } else if (path.equals(eBankingPath)) {
-            view?.loadUrl(url)
-        } else if (path.equals(mPinPath) || fragment.equals(mPinPath)) {
-            val deeplink = "https://khalti.com/go/?t=mpin"
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(deeplink))
-
-            activity.startActivity(browserIntent)
-        } else {
-            Toast.makeText(activity, "Action not permitted", Toast.LENGTH_SHORT).show()
-        }
-
-        return true
     }
 }
