609e2439ced373f24ea57183f16c34e22c2bd657
Ishwor-Shrestha <ishworisgod@gmail.com>
2024-02-22 11:34:20 +0545

    Separate config fields into separate data class for separation of concerns and easy parcelization

---
+++ khalti-android/src/main/java/com/khalti/android/v3/KhaltiPayConfig.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024. The Khalti Authors. All rights reserved.
+ */
+
+package com.khalti.android.v3
+
+import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class KhaltiPayConfig(
+    val publicKey: String,
+    val pidx: String,
+    val returnUrl: Uri,
+    val openInKhalti: Boolean = true,
+    val environment: Environment = Environment.PROD,
+) : Parcelable
