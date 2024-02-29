c2bd6ede29813147c7c7a3b1ad0ab1c7fa4de62d
Ishwor-Shrestha <ishworisgod@gmail.com>
2024-02-29 18:28:45 +0545

    Cleanup import

--- khalti-android/src/main/java/com/khalti/android/service/VerificationService.kt
+++ khalti-android/src/main/java/com/khalti/android/service/VerificationService.kt
@@ -9,7 +9,6 @@ import com.khalti.android.api.ApiService
 import com.khalti.android.api.safeApiCall
 import com.khalti.android.resource.KFailure
 import com.khalti.android.resource.Result
-import com.khalti.android.v3.KhaltiPayConfig
 import com.khalti.android.v3.PaymentPayload
 import kotlinx.coroutines.Dispatchers
 
