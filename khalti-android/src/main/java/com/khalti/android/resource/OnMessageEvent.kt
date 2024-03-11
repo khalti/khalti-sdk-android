bd393d44a832261b0842cee80c63a7db82362963
Ishwor-Shrestha <ishworisgod@gmail.com>
2024-03-11 14:55:40 +0545

    Add message event types for onMessage

---
+++ khalti-android/src/main/java/com/khalti/android/resource/OnMessageEvent.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024. The Khalti Authors. All rights reserved.
+ */
+
+package com.khalti.android.resource
+
+enum class OnMessageEvent {
+    BackPressed, ReturnUrlLoadFailure, NetworkFailure, PaymentLookUpFailure, Unknown
+}
